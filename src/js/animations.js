af31cc73ca5c0d6e785d1535f6cbd69b5e6680cf
Sleek9 <waynesleek3@gmail.com>
2021-09-17 19:56:29 -0500

    .

--- src/js/animations.js
+++
@@ -1,7 +0,0 @@
-const $bg = document.querySelectorAll(".bg"); //Background inicio
-
-window.addEventListener("scroll", () => {
-  const y = window.scrollY;
-  $bg[0].style.transform = `translate(0px, ${-y / 20}%)`;
-  $bg[1].style.transform = `translate(0px, ${-y / 60}%)`;
-});
